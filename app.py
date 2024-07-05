6934e3ce94ebf47ffbcd892edb9861a1f0f724dd
Sourab M Kalliyan <sourabmkalliyanofficial@gmail.com>
2024-07-06 05:28:16 +0530

    updated general settings

--- app.py
+++ app.py
@@ -8,3 +8,20 @@ current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd() # 
 css_file = current_dir / "styles" / "main.css" # Path to the CSS file
 resume_file = current_dir / "assets" / "resume.pdf" # Path to the resume file
 profile_pic = current_dir / "assets" / "me.jpg" # Path to the profile picture
+
+
+# --- GENERAL SETTINGS ---
+
+PAGE_TITLE = "Kalliyan"
+PAGE_ICON = ":wave:"
+NAME = "Sourab M Kalliyan"
+DESCRIPTION = "Ethical Hacker | Cyber Security Enthusiast | Python Developer"
+EMAIL = "sourabmkalliyanofficial@gmail.com"
+SOCIAL_MEDIA = {
+    "LinkedIn": "https://www.linkedin.com/in/kalliyan/",
+    "GitHub": "https://github.com/sourabmkalliyan",
+}
+
+PROJECTS = {
+    "MAC SHIFT: MAC Address Changer": "https://github.com/sourabmkalliyan/mac-shift ",
+}
